28657a32d4f80db1aa4434f6a48558509ec3e2e8
Shraddha Pai <shraddha.pai@utoronto.ca>
2020-09-24 13:25:51 -0400

    readme + supporting files + doc

--- school-scraper/mergeMaster.sh
+++ school-scraper/mergeMaster.sh
@@ -2,6 +2,19 @@
 
 # auto get new cases from GTA 
 
+# findNewRecords.R - compares current to last snapshot (1700 EDT daily) and
+# collects records with differences.
+#
+# annotNewRecords.R - converts the new records into the csv format needed
+# for the Google map. Uses Ontario school lat/long info to auto-populate
+# those fields and adds entries for other fields found in Google map
+# 
+# Note on args: Each school has two args associated with it:
+# 1) the folder name (no spaces) : e.g. "Toronto_DSB"
+# 2) name of school board: must match school board name in Ontario-wide
+# list of lat/long
+
+
 #Rscript findNewRecords.R Toronto_DSB
 #Rscript annotNewRecords.R Toronto_DSB "Toronto DSB"
 
