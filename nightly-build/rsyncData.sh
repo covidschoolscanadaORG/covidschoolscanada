5eb3affc221616dab60a2c1c6b0a71ec044dc761
Shraddha Pai <shraddha.pai@utoronto.ca>
2020-10-10 07:13:06 -0400

    rsyncs to google drive

--- nightly-build/rsyncData.sh
+++ nightly-build/rsyncData.sh
@@ -1,5 +1,5 @@
 # run on SP machine
 
 srcDir=/home/shraddhapai/Canada_COVID_tracker
-tgtDir="/Users/shraddhapai/Google/Advocacy/COVID-19/daily_data"
+tgtDir="/Users/shraddhapai/Google_covidschools/daily_data"
 rsync -ravz shraddhapai@192.168.81.205:${srcDir} $tgtDir
