965aa66f6c6289bc1ee55420ad0858e1ecd72d8e
shraddhapai <shraddha.pai@utoronto.ca>
2020-10-12 07:13:21 -0400

    removed

--- nightly-build/rsyncData.sh
+++
@@ -1,5 +0,0 @@
-# run on SP machine
-
-srcDir=/home/shraddhapai/Canada_COVID_tracker
-tgtDir="/Users/shraddhapai/Google/Advocacy/COVID-19/daily_data"
-rsync -ravz shraddhapai@192.168.81.205:${srcDir} $tgtDir
