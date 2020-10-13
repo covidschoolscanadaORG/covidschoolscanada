516d820aab44d09a494cea198076cd8e76a65c50
shraddhapai <shraddha.pai@utoronto.ca>
2020-10-12 23:59:44 -0400

    adds CEQ annotation sheet data

---
+++ nightly-build/fetchQCstats.R
@@ -0,0 +1,16 @@
+# download CovidEcoles sheet with school annotation
+
+sheetURL <- "https://docs.google.com/spreadsheets/d/1Il2Xhz-VKhME4B9EqKkzbmT5APf3IaKktyk80SQi6JA/edit#gid=154085736"
+
+dt <- format(Sys.Date(),"%y%m%d")
+outDir <- sprintf("/home/shraddhapai/Canada_COVID_tracker/export-%s",dt)
+
+require(gsheet)
+message("* Fetching CEQ annotation")
+x <- gsheet2tbl(sheetURL)
+y <- as.data.frame(x)
+outFile <- sprintf("%s/CEQ_annotated_%s.csv",outDir,dt)
+message("* Writing local copy")
+write.table(y,file=outFile,sep=",",col=TRUE,row=FALSE,
+	quote=TRUE)
+
---
+++ nightly-build/qcStats.R
@@ -0,0 +1,80 @@
+# convert qc update excel file into format compatible
+# with covidschoolsCA
+
+dt <- format(Sys.Date(),"%y%m%d")
+outDir <- sprintf("/home/shraddhapai/Canada_COVID_tracker/export-%s",dt)
+
+inFile <- sprintf("%s/CEQ_annotated_%s.csv",outDir,dt)
+outFile <- sprintf("%s/CEQ_annotated_clean_%s.csv",outDir,dt)
+
+dat <- read.delim(inFile,sep=",",h=T,as.is=T)
+
+for (k in 1:ncol(dat)) {
+	dat[,k] <- stringr::str_trim(dat[,k])
+}
+
+
+map_colname <- list(
+	"École"="institute.name",
+	"Nombre"="Total.cases.to.date",
+	"Ville"="City",
+	"Type.d.établissement"="Type_of_school",
+	"Centre.de.Service"="School.board",
+	"latitude"="Latitude",
+	"longitude"="Longitude")
+for (nm in names(map_colname)){
+	colnames(dat)[which(colnames(dat)==nm)] <- map_colname[[nm]]
+}
+dat$Total.students.to.date <- NA
+dat$Total.staff.to.date <- NA
+
+dat$Total.outbreaks.to.date <- NA
+dat$Outbreak.dates <- NA
+dat$Outbreak.Status <- "Single/unlinked cases"
+
+idx <- which(dat$Total.cases.to.date >=5)
+dat$Total.outbreaks.to.date[idx] <- 1L
+dat$Outbreak.Status[idx] <- "Declared outbreaks"
+dat$Outbreak.dates[idx] <- dat$Date[idx]
+
+dat$Province <- "QC"
+
+finalorder <- c("institute.name","Total.cases.to.date",
+	"Total.students.to.date","Total.staff.to.date",
+	"Date","Article",
+	"Total.outbreaks.to.date","Outbreak.dates","Outbreak.Status",
+	"Type_of_school","School.board",
+	"City","Province",
+	"Latitude","Longitude")
+
+
+dat$Article <- "https://www.covidecolesquebec.org/nouvelles-closions"
+dat  <- dat[,finalorder]
+
+# --------------------------------------------
+# Clean school type
+# --------------------------------------------
+dat$Type_of_school[which(dat$Type_of_school %in% 
+	c("Secondaire, CEGEP, Professionnel",
+	"Professionnel",
+	"professionnel","Université","CEGEP"))] <- "Post-secondary"
+dat$Type_of_school <- sub("Primaire et Secondaire","Mixed",dat$Type_of_school)
+dat$Type_of_school[grep("et secondaire",dat$Type_of_school)] <- "Mixed"
+dat$Type_of_school <- sub("Primaire","Elementary",dat$Type_of_school)
+dat$Type_of_school <- sub("Secondaire","Secondary",dat$Type_of_school)
+dat$Type_of_school <- sub("Commission scolaire","Field Office",dat$Type_of_school)
+dat$Type_of_school[grep("^Secondary s",dat$Type_of_school)] <- "Secondary"
+print(table(dat$Type_of_school))
+
+
+# --------------------------------------------
+# Clean region
+# --------------------------------------------
+dat$School.board <- sub("Decouvrers","Découvreurs",dat$School.board)
+dat$School.board <- sub("Energie","Énergie",dat$School.board)
+dat$School.board <- sub("Jonquiere","Jonquière",dat$School.board)
+dat$School.board <- sub("Vallee des Tisserands","Vallée-des-Tisserands",
+	dat$School.board)
+
+write.table(dat,file=outFile,sep=",",col=T,row=F,quote=TRUE)
+
