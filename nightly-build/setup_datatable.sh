f5a0b3106ed2855697f0dc9e85c98477a370fa4a
shraddhapai <shraddha.pai@utoronto.ca>
2020-10-16 16:29:05 -0400

    works with multi-layer map + added Perl parsing for CDATA tags ending abruptly

--- nightly-build/setup_datatable.sh
+++ nightly-build/setup_datatable.sh
@@ -5,6 +5,8 @@ outDir=$1
 mid=$2 # ID for midfile
 outName=$3
 
+lid="y84_ssX2HbU"
+
 echo $outDir
 echo $mid
 echo $outName
@@ -12,15 +14,24 @@ echo $outName
 mkdir -p ${outDir}/backup_old
 mv ${outDir}/*kml ${outDir}/backup_old/.
 
-fetchUrl="https://www.google.com/maps/d/u/0/kml?mid=${mid}&forcekml=1"
+#fetchUrl="https://www.google.com/maps/d/u/0/kml?mid=${mid}&cid=mp&forcekml=1"
+fetchUrl="https://www.google.com/maps/d/u/0/kml?mid=${mid}&cid=mp&cv=53DpL5LnT98.en."
 wget "$fetchUrl"
 wait
 
-dummy="kml?mid=${mid}&forcekml=1"
-mv $dummy ${outDir}/${outName}.kml
+##dummy="kml?mid=${mid}&cid=mp&forcekml=1"
+dummy="kml?mid=${mid}&cid=mp&cv=53DpL5LnT98.en."
+mv $dummy ${outDir}/${outName}.kmz
+###mv $dummy ${outDir}/${outName}.kml
+curd=`pwd`
+cd $outDir
+unzip ${outDir}/${outName}.kmz
+mv ${outDir}/doc.kml ${outName}.kml
+rm -r ${outDir}/images
+cd $curd
 # strip the CDATA tags that prevent some names from registering
 # in the parser
-perl -pe 's/<\!\[CDATA\[(.*)\]\]>/$1/' ${outDir}/${outName}.kml > ${outDir}/${outName}_clean.kml
+perl -pe 's/<\!\[CDATA\[(.*)\]\]>/$1/' ${outDir}/${outName}.kml | perl -pe 's/<\!\[CDATA\[(.*)/$1/' | perl -pe 's/^\]\]//'> ${outDir}/${outName}_clean.kml
 
 perl -pe 's/<description>(.*)<\/description>//' ${outDir}/${outName}_clean.kml > ${outDir}/${outName}_clean2.kml
 perl -pe 's/&/&amp;/g' ${outDir}/${outName}_clean2.kml > ${outDir}/${outName}_clean3.kml
