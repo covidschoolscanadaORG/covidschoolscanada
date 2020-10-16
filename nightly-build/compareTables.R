821259748561f526f418f49acd5157b3e986f69e
shraddhapai <shraddha.pai@utoronto.ca>
2020-10-15 22:17:47 -0400

    compare dates to resolve Google issue

---
+++ nightly-build/compareTables.R
@@ -0,0 +1,13 @@
+# compare tables from different days
+
+getTable <- function(dt) {
+	message(sprintf("Fetching %s",dt))
+	inDir <- sprintf("/home/shraddhapai/Canada_COVID_tracker/export-%s",dt)
+	inFile <- sprintf("%s/CanadaMap_QuebecMerge-%s.clean.csv",
+		inDir,dt)
+	dat <- read.delim(inFile,sep=",",h=T,as.is=T)
+	return(dat)
+}
+
+today <- getTable("201015")
+
