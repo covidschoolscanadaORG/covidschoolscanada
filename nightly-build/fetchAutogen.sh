8b381d9786cbff3dd47c8d79fdbcdae0285c3419
shraddhapai <shraddha.pai@utoronto.ca>
2020-11-01 08:56:35 -0500

    minor

--- nightly-build/fetchAutogen.sh
+++ nightly-build/fetchAutogen.sh
@@ -1,10 +1,10 @@
 # fetch auto-gen table
-dt2=$(date +%Y-%m-%d -d "$(date) - 1 day")
+dt2=$(date +%Y-%m-%d -d "$(date)")
 #dt2=$(date +%Y-%m-%d)-1
 echo $dt2
 baseURL=https://covidschoolboards.s3.ca-central-1.amazonaws.com
 tgtDir=/home/shraddhapai/Canada_COVID_tracker/AutoGen
-inFile=${baseURL}/Peel_${dt2}.csv
+inFile=${baseURL}/Automated_boards_${dt2}.csv
 echo $inFile
 wget $inFile
 baseF=`basename $inFile`
