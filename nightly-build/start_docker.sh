8f97092f064c040827f6bebfde85038e86ed3a51
shraddhapai <shraddha.pai@utoronto.ca>
2020-11-17 08:09:22 -0500

    uses covidschools docker

--- nightly-build/start_docker.sh
+++ nightly-build/start_docker.sh
@@ -3,4 +3,4 @@
 docker run -it \
 	-v /home/shraddhapai/Canada_COVID_tracker:/home/shraddhapai/Canada_COVID_tracker \
 	-v /home/shraddhapai/software/covidschoolscanada:/home/shraddhapai/software/covidschoolscanada \
-	bioc312_netdx /bin/bash
+	shraddhapai/covidschoolscanada:latest /bin/bash
